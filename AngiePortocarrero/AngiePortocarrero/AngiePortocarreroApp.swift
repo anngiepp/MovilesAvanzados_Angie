246bb068e078a810d71bd5bcf4128eac9fb94d81
Alexis <angie.portocarrero@tecsup.edu.pe>
2026-08-26 09:44:17 -0500

    Implementar aplicacion de pagos

---
+++ AngiePortocarrero/AngiePortocarrero/AngiePortocarreroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngiePortocarreroApp.swift
+//  AngiePortocarrero
+//
+//  Created by Tecsup on 26/08/26.
+//
+
+import SwiftUI
+
+@main
+struct AngiePortocarreroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
