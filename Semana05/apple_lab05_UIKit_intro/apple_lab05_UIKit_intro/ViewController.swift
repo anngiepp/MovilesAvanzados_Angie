867099d4a7c72613143a8a3a3ffdc78e92d59057
anngiepp <angie.portocarrero@tecsup.edu.pe>
2026-09-16 10:09:54 -0500

    feat: Estructura completa

--- Semana05/apple_lab05_UIKit_intro/apple_lab05_UIKit_intro/ViewController.swift
+++ Semana05/apple_lab05_UIKit_intro/apple_lab05_UIKit_intro/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var heightTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func CalcularResultado(_ sender: Any) {
+        guard let weightText = weightTextField.text, let weight = Double(weightText), weight > 0,
+              let heightText = heightTextField.text, let height = Double(heightText), height > 0 else {
+            resultLabel.text = "Ingrese valores válidos"
+            return
+        }
+        
+        let bmi = weight / (height * height)
+        
+        var status = ""
+        if bmi < 18.5 {
+                    status = "Bajo peso"
+        } else if bmi <= 24.9 {
+                    status = "Peso normal"
+        } else if bmi <= 29.9 {
+                    status = "Sobrepeso"
+        } else {
+            status = "Obesidad"
+        }
+        
+        resultLabel.text = String(format: "IMC: %.2f - %@", bmi, status)
+    }
+    
 
 }
 
